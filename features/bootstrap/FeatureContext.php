a118cece111bf870057e870a591506108a9f7a61
Lise Poirier <lise.p.poirier@gmail.com>
2016-09-19 10:04:41 +0200

    Mise en place des fichiers de class

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -4,6 +4,7 @@ use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Tester\Exception\PendingException;
 
 /**
  * Defines application features from the specific context.
