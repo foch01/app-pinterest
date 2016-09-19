26001b45cff5afe032ffd915bb0d8ace68e4c363
Redfish <lesonduko@gmail.com>
2016-09-19 17:19:35 +0200

    resolution de la condition testuser true or false

--- features/bootstrap/Entity/User.php
+++ features/bootstrap/Entity/User.php
@@ -3,31 +3,41 @@ namespace Entity;
 
 class User
 {
-  private $_id;
-  private $_pseudo;
-  private $_mail;
-  private $_pwd;
-  private $_key;
+      private $_id;
+      private $_pseudo;
+      private $_mail;
+      private $_pwd;
+      private $_key;
 
-  public function __construct($pseudo, $pwd){
-    $this->_pseudo = $pseudo;
-    $this->_pwd = $pwd;
+      public function __construct($pseudo, $pwd){
+        $this->_pseudo = $pseudo;
+        $this->_pwd = $pwd;
+      }
+      public function connect(){
+        
+        $dsn = 'mysql:dbname=mixterest; host=127.0.0.1';
+        $user ='red';
+        $password = 'admin';
+        $dbh = new \PDO($dsn, $user, $password);
+        $sql = "select * from user where pseudo = '$this->_pseudo' and pass='$this->_pwd'";
+          
+        $result = $dbh->query($sql);
+        print_r($result->fetchAll(\PDO::FETCH_NAMED));
+
+        return $result;
+      }
   }
-  public function connect(){
-    
-    $dsn = 'mysql:dbname=mixterest; host=127.0.0.1';
-    $user ='red';
-    $password = 'admin';
-    $dbh = new \PDO($dsn, $user, $password);
-    $sql = "select * from user where pseudo = '$this->_pseudo' and pass='$this->_pwd'";
-      echo $sql;
-    $result = $dbh->query($sql);
-    echo count($result);
 
-    return $result;
+public function validatemail () {
 
+  if (preg_match(^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$)){
+echo 'email valide';
   }
-
-
+else {
+  echo 'email invalide';
 }
+}
+
+
+
 ?>
