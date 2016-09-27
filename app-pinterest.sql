11e8261659b7d7bcc2df8495feb179b45088650c
erwannFace <e.duclos@fondationface.org>
2016-09-27 14:50:29 +0200

    updating database

--- app-pinterest.sql
+++ app-pinterest.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 26, 2016 at 01:51 PM
--- Server version: 5.7.13-0ubuntu0.16.04.2
--- PHP Version: 7.0.8-0ubuntu0.16.04.2
+-- Generation Time: Sep 27, 2016 at 02:49 PM
+-- Server version: 5.5.52-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,31 +14,13 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `app-pinterest`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `SuperUser`
---
-
-CREATE TABLE `SuperUser` (
-  `id` int(255) NOT NULL,
-  `pseudo` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `pass` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `mail` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `SuperUser`
---
-
-INSERT INTO `SuperUser` (`id`, `pseudo`, `pass`, `mail`) VALUES
-(1, 'testSuperAdmin', 'facesimplon', 'facesimplon@facesimplon.com');
+CREATE DATABASE IF NOT EXISTS `app-pinterest` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+USE `app-pinterest`;
 
 -- --------------------------------------------------------
 
@@ -46,12 +28,13 @@ INSERT INTO `SuperUser` (`id`, `pseudo`, `pass`, `mail`) VALUES
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `pseudo` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `pass` varchar(50) NOT NULL,
-  `mail` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `mail` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
 
 --
 -- Dumping data for table `user`
@@ -60,36 +43,6 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `pseudo`, `pass`, `mail`) VALUES
 (1, 'testuser', 'nnnn', 'reddeveloppement@wanalike.fr');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `SuperUser`
---
-ALTER TABLE `SuperUser`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `SuperUser`
---
-ALTER TABLE `SuperUser`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
